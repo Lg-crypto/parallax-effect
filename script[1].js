50f4277dc8eeb7691c7633ffdd9e5807bd3963ff
Halejandro_Oliveira <122477963+Lg-crypto@users.noreply.github.com>
2024-02-22 15:19:55 -0300

    Add files via upload

---
+++ script[1].js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal({reset: true})
+
+
+sr.reveal('.area-1',{duration: 1000})
+sr.reveal('.area-2',{
+    duration: 2000,
+    rotate:{x: 0, y:80, z:0}
+})
+sr.reveal('.area-3',{
+    duration: 2000,
+    rotate:{x: 100, y:100, z:0}
+})
+
+sr.reveal('.module',{duration: 1000})
+
+
+
+
+
+document.addEventListener("DOMContentLoaded",function () {
+  setTimeout(function () {
+   
+   
+  document.getElementsByClassName("loading")[0].style.display="none";
+  document.getElementsByClassName("box")[0].style.display="block";
+  
+  },3000)
+
+
+})
